2027d3bd769364a06e51cd057929bd1c473d6f4f
Lee Forest <lee8oi@gmail.com>
2014-05-27 12:21:13 -0400

    script now supports 32 & 64-bit and uses official download source

--- client-install.sh
+++ client-install.sh
@@ -1,10 +1,30 @@
-# Grab Teamspeak client and icon image
-wget http://hellhawks.net/teamspeak/TeamSpeak3-Client-linux_amd64.run
-wget http://hellhawks.net/teamspeak/teamspeak-icon.png
+#!/bin/bash
+#
+# EasyTS3 client-install by Lee Forest (lee8oi@gmail.com)
+#
+# A simple script that performs a system-wide installation of the TeamSpeak3 client.
 
+# Get architecture.
+case `uname -m` in
+	i686)
+		arch=x86
+		;;
+	x86_64)
+		arch=amd64
+		;;
+	*)
+		echo "cannot determine architecture"
+		exit
+esac
+
+# Grab Teamspeak client and icon image
+wget http://dl.4players.de/ts/releases/3.0.14/TeamSpeak3-Client-linux_$arch-3.0.14.run
+wget https://raw.githubusercontent.com/lee8oi/easyts3/master/teamspeak3.png
+
+file=TeamSpeak3-Client-linux_$arch-3.0.14.run
 # Run TeamSpeak self extracting file
-chmod +x TeamSpeak3-Client-linux_amd64.run
-./TeamSpeak3-Client-linux_amd64.run
+chmod +x $file
+./$file
 
 # Create teamspeak.desktop file
 echo "[Desktop Entry]" >> teamspeak.desktop
@@ -15,15 +35,15 @@ echo "Comment=Voice chat for gamers." >> teamspeak.desktop
 echo "Type=Application" >> teamspeak.desktop
 echo "Categories=Internet" >> teamspeak.desktop
 echo "Terminal=false" >> teamspeak.desktop
-echo "Icon=/usr/local/share/TeamSpeak3-Client-linux_amd64/teamspeak-icon.png" >> teamspeak.desktop
-echo "Exec=/usr/local/share/TeamSpeak3-Client-linux_amd64/ts3client_runscript.sh" >> teamspeak.desktop
+echo "Icon=/usr/local/share/TeamSpeak3-Client-linux_$arch/teamspeak3.png" >> teamspeak.desktop
+echo "Exec=/usr/local/share/TeamSpeak3-Client-linux_$arch/ts3client_runscript.sh" >> teamspeak.desktop
 
 # Install TeamSpeak system-wide
-sudo mv TeamSpeak3-Client-linux_amd64/ /usr/local/share/
-sudo mv teamspeak-icon.png /usr/local/share/TeamSpeak3-Client-linux_amd64/
-sudo ln -s /usr/local/share/TeamSpeak3-Client-linux_amd64/ts3client_runscript.sh /usr/local/bin/teamspeak
+sudo mv TeamSpeak3-Client-linux_$arch/ /usr/local/share/
+sudo mv teamspeak3.png /usr/local/share/TeamSpeak3-Client-linux_$arch/
+sudo ln -s /usr/local/share/TeamSpeak3-Client-linux_$arch/ts3client_runscript.sh /usr/local/bin/teamspeak
 sudo mv teamspeak.desktop /usr/share/applications/
 
 # Remove .run file
-rm TeamSpeak3-Client-linux_amd64.run
+rm $file
 
