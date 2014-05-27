3cc49e21c22d8999ae107734d48bc11d0e80b4e7
Lee Forest <lee8oi@gmail.com>
2014-05-27 12:20:09 -0400

    added header/description

--- client-remove.sh
+++ client-remove.sh
@@ -1,1 +1,8 @@
-sudo rm -rf /usr/local/bin/teamspeak /usr/local/share/TeamSpeak3-Client-linux_amd64 /usr/share/applications/teamspeak.desktop
+#!/bin/bash
+#
+# EasyTS3 client-install by Lee Forest (lee8oi@gmail.com)
+#
+# A simple script that performs a system-wide removal of the TeamSpeak3 client
+# (If it was installed by EasyTS3 client-install.sh).
+
+sudo rm -rf /usr/local/bin/teamspeak /usr/local/share/TeamSpeak3-Client-linux_* /usr/share/applications/teamspeak.desktop
